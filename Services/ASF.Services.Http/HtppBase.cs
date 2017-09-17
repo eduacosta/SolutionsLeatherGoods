ccef8a5f8dd816ea853daa2589c603899a36e99a
Eduardo Ariel Acosta <eduacosta22@hotmail.com>
2017-09-17 15:56:53 -0300

    sdfdsf

---
+++ Services/ASF.Services.Http/HtppBase.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using ASF.Business.FachadaBLL;
+using ASF.Entities;
+using ASF.Services.Contracts;
+
+namespace ASF.Services.Http
+{
+    public abstract class HtppBase <T> where T : EntityBase
+    {
+
+        [HttpPost]
+        [Route("Add")]
+        public abstract T Add(T entidad);
+
+
+        [HttpGet]
+        [Route("All")]
+        public abstract AllResponse All();
+
+
+        [HttpPost]
+        [Route("Edit")]
+        public abstract void Edit(Category category);
+       
+
+        [HttpGet]
+        [Route("Find")]
+        public abstract FindResponse Find(int id)
+       
+
+        [HttpPost]
+        [Route("Remove")]
+        public void Remove(Category category)
+        {
+            try
+            {
+                var bc = FachadaBLL.CategoryBusiness;
+                bc.Delete(new Category() { Id = category.Id });
+            }
+            catch (Exception ex)
+            {
+                var httpError = new HttpResponseMessage()
+                {
+                    StatusCode = (HttpStatusCode)422,
+                    ReasonPhrase = ex.Message
+                };
+
+                throw new HttpResponseException(httpError);
+            }
+        }
+
+    }
+}
